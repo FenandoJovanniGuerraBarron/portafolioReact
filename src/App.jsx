4c0ad22763e7cc25cb5c89dbdb0d9c23fef26436
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-12-30 21:44:21 -0600

    uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import CardInfo from './components/CardInfo'
 import Header from './components/Header'
